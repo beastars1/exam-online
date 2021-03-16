8e9f6aaf9b0ba855956f79e28c7d47f9923e6e5e
beastars1 <1229435236@qq.com>
2021-03-16 23:06:10 +0800

    refactor entity

---
+++ src/main/java/com/lzq/exam/common/Question.java
@@ -0,0 +1,13 @@
+package com.lzq.exam.common;
+
+/**
+ * 所有问题的父类
+ *
+ * @author beastars
+ */
+public class Question {
+  protected Long id;
+  protected String question;
+  protected String answer;
+  protected Integer score;
+}
---
+++ src/main/java/com/lzq/exam/common/Role.java
@@ -0,0 +1,12 @@
+package com.lzq.exam.common;
+
+/**
+ * 所有人员的父类
+ *
+ * @author beastars
+ */
+public class Role {
+  protected Long id;
+  protected Long name;
+  protected Integer role;
+}
